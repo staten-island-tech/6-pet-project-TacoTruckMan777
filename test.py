7e8e2e8ac22439c4908c56fec98232c0959d7877
TacoTruckMan777 <jcruz91011@gmail.com>
2025-11-13 09:19:29 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,22 @@
 
 
 
+def ratio(number):
+    positive = 0
+    negative = 0
+    zero = 0
+    count = len(number)
+    for i in range(count):
+        if i > 0:
+            positive += 1
+        elif i < 0:
+            negative += 1
+        else:
+            zero += 1
+    pos = float(positive/count)
+    neg = float(negative/count)
+    ze = float(zero/count)
+
+    print(f"positive:{pos}, negative:{neg}, zero:{ze}")
+
+ratio([1,2,0,-4,-5])
