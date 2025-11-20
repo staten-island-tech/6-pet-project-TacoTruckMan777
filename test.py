b1bc10082d1b41bdb07d59eeefa39e28b433cb94
TacoTruckMan777 <jcruz91011@gmail.com>
2025-11-20 09:26:23 -0500

    stufffff

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
-
-
-
-
-
-
-
-f
+class Player:
+    def __init__(self, name, Runes, StartingClass, inventory, Vigor, Mind, Endurance, Strength, Dex, IQ, Faith, Arcane ):
+        self.name = name
+        self.Runes = Runes
+        self.inventory = inventory
+        self.StartingClass = StartingClass
+        self.Vigor = Vigor
+        self.Mind = Mind
+        self.Endurance = Endurance
+        self.Strength = Strength
+        self.Dex = Dex
+        self.IQ = IQ
+        self.Faith = Faith
+        self.Arcane = Arcane
+        
+    def StartingClass(Hero, Bandit, Astrologer, Warrior, Prisoner, Confessor, Wretch, Vagabond, Prophet, Samurai, Heavy Knight):
+        
