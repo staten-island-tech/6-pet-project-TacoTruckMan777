e3952c96bf065cc3a140c90943015d5d52d72e2f
TacoTruckMan777 <jcruz91011@gmail.com>
2025-11-25 09:25:50 -0500

    more stuff XD

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 class Player:
-    def __init__(self, name, Runes, StartingClass, inventory, Vigor, Mind, Endurance, Strength, Dex, IQ, Faith, Arcane ):
+    def __init__(self, name, Runes, inventory, Vigor, Mind, Endurance, Strength, Dex, IQ, Faith, Arcane ):
         self.name = name
-        self.Runes = Runes
+        self.Runes = 500
         self.inventory = inventory
-        self.StartingClass = StartingClass
         self.Vigor = Vigor
         self.Mind = Mind
         self.Endurance = Endurance
@@ -12,7 +11,47 @@ class Player:
         self.IQ = IQ
         self.Faith = Faith
         self.Arcane = Arcane
-        
-    def StartingClass(Hero, Bandit, Astrologer, Warrior, Prisoner, Confessor, Wretch, Vagabond, Prophet, Samurai, Heavy Knight):
-         
-        
+
+class Hero(Player):
+    def __init__(self, name, Runes, inventory, Vigor, Mind, Endurance, Strength, Dex, IQ, Faith, Arcane ):
+        self.name = name
+        self.Runes = 500
+        self.inventory = inventory
+        self.Vigor = 14
+        self.Mind = 9
+        self.Endurance = 12
+        self.Strength = 16
+        self.Dex = 9
+        self.IQ = 7
+        self.Faith = 8
+        self.Arcane = 11
+
+class Bandit(Player):
+    def __init__(self, name, Runes, inventory, Vigor, Mind, Endurance, Strength, Dex, IQ, Faith, Arcane ):
+        self.name = name
+        self.Runes = 500
+        self.inventory = inventory
+        self.Vigor = 10
+        self.Mind = 11
+        self.Endurance = 10
+        self.Strength = 9
+        self.Dex = 13
+        self.IQ = 9
+        self.Faith = 8
+        self.Arcane = 14
+    
+class Astrologer(Player):
+    def __init__(self, name, Runes, inventory, Vigor, Mind, Endurance, Strength, Dex, IQ, Faith, Arcane ):
+        self.name = name
+        self.Runes = 500
+        self.inventory = inventory
+        self.Vigor = 9
+        self.Mind = 15
+        self.Endurance = 9
+        self.Strength = 8
+        self.Dex = 12
+        self.IQ = 16
+        self.Faith = 7
+        self.Arcane = 9
+    
+    
